d6f8df90adc269eaf5bc85665bab31588d4d95b1
ganzonglei <ganzonglei@bonc.com.cn>
2022-02-28 17:40:59 +0800

    新增登陆页以及404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,23 @@ import Home from "../views/Home.vue";
 
 const routes = [
   {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+  },
+  {
     path: "/",
     name: "Home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: import(/* webpackChunkName: "404" */ "../views/NotFound.vue"),
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <span class="item">4</span>
+    <span class="item">0</span>
+    <span class="item">4</span>
+    <div>
+      <el-link type="info" :underline="false">页面不存在!</el-link>&nbsp;&nbsp;
+      <el-link type="danger" @click="goToBack()">返回</el-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  methods: {
+    goToBack: function () {
+      this.$router.back();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  padding-top: 150px;
+  text-align: center;
+
+  span.item {
+    display: inline-block;
+    font-size: 130px;
+    font-weight: bold;
+    padding: 0 15px;
+    text-shadow: 0 0 15px rgb(0, 0, 0, 0.4);
+    color: #fff;
+  }
+}
+</style>
