e9d7a6ee372414ca4538cc0d6704f2ca9b3dc770
ouheiguai9 <349382440@qq.com>
2022-03-06 22:58:47 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App)
+const app = createApp(App)
   .use(ElementPlus, {
     size: 'small',
     zIndex: 3000,
   })
+  .use(VueAxios, axios)
   .use(store)
-  .use(router)
-  .mount('#app');
+  .use(router);
+app.mount('#app');
