51b6cb90e3de7d32b931ec42835245855b2c87b7
ouheiguai9 <349382440@qq.com>
2022-03-06 23:26:23 +0800

    ElementPlus设置国际化

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 const app = createApp(App)
   .use(ElementPlus, {
+    locale: zhCn,
     size: 'small',
     zIndex: 3000,
   })
