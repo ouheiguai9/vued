214b9c497f0394748cd68434d47cdb18d2f1ad54
ouheiguai9 <349382440@qq.com>
2022-03-06 23:01:16 +0800

    调整登录页

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <router-view />
 </template>
-
 <style lang="scss">
-#app {
-  font-family: 微软雅黑, Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  font-size: 14px;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>Login</div>
+  <el-card class="login-card">
+    <template #header>
+      <div class="card-header">
+        Vue3 Demo
+      </div>
+    </template>
+    <el-form :model="form" size="large">
+      <el-form-item>
+        <el-input v-model="form.username" placeholder="请输入用户名" :prefix-icon="User" />
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          v-model="form.password" placeholder="请输入密码" show-password
+          :prefix-icon="Key"
+        />
+      </el-form-item>
+      <div class="btn-box">
+        <el-button type="primary" @click="onLogin">
+          登录
+        </el-button>
+        <el-button @click="onRegister">
+          注册
+        </el-button>
+      </div>
+    </el-form>
+  </el-card>
 </template>
-
+<script setup>
+import {User, Key} from '@element-plus/icons-vue';
+</script>
 <script>
 export default {
   name: 'LoginView',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+
+      }
+    }
+  },
+  methods: {
+    onLogin() {
+    },
+    onRegister() {
+
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.login-card {
+  width: 400px;
+  margin: 280px auto 0;
+  font-size: var(--el-font-size-extra-large);
+}
+
+.btn-box {
+  width: 100%;
+  text-align: center;
+}
+</style>
