d6a44ef8594a05888547bfc7089fff78291c5d09
ouheiguai9 <349382440@qq.com>
2022-03-30 00:31:38 +0800

    增加国际化插件并调整相关功能

---
+++ src/i18n/en.js
@@ -0,0 +1,8 @@
+import ElementPlusLocale from 'element-plus/es/locale/lang/en'
+export default {
+  app: {
+    name: 'VUE DEMO',
+  },
+  element: ElementPlusLocale,
+  desc: 'English',
+}
---
+++ src/i18n/zh-cn.js
@@ -0,0 +1,8 @@
+import ElementPlusLocale from 'element-plus/es/locale/lang/zh-cn'
+export default {
+  app: {
+    name: 'VUE前端工程',
+  },
+  element: ElementPlusLocale,
+  desc: '简体中文',
+}
